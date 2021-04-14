d27f5f9f03bad10a51b6907435c76d13bad007d4
Lucas Knudsen <laksteelhouse@gmail.com>
2021-04-14 20:46:58 +0200

    Setting up app

---
+++ components/ObjectRenderer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ObjectRenderer = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default ObjectRenderer
+
+const styles = StyleSheet.create({})
